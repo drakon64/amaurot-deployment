6a30c59e04bd6c71c3b207793017cbe95ae41865
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-22 19:57:37 +0000

    Add Firestore

---
+++ amaurot/firestore.tf
@@ -0,0 +1,17 @@
+resource "google_project_service" "firestore" {
+  service = "firestore.googleapis.com"
+}
+
+resource "google_firestore_database" "firestore" {
+  location_id = var.region
+  name        = "(default)"
+  type        = "FIRESTORE_NATIVE"
+  
+  depends_on = [google_project_service.firestore]
+}
+
+resource "google_project_iam_member" "firestore" {
+  member  = google_service_account.processor.member
+  project = data.google_project.project.id
+  role    = "roles/datastore.user"
+}
