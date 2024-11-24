297455762122776c39e5f998887c4cd1d0cf36e1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-24 17:41:31 +0000

    Add workload identity provider to outputs

--- amaurot/outputs.tf
+++ amaurot/outputs.tf
@@ -1,3 +1,7 @@
 output "receiver_uri" {
   value = "${google_cloud_run_v2_service.receiver[0].uri}/api/github/webhooks"
 }
+
+output "workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github_actions.name
+}
